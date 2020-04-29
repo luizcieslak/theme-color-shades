fdc5eb7aa88bb51ce60dec9def85b1f85b7f5627
luizcieslak <cieslakluiz@gmail.com>
2020-04-29 15:24:55 -0300

    chore(backend): testing tinyColor

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,1 +1,25 @@
-console.log('oba')
+const fs = require('fs')
+const readline = require('readline')
+
+import { join as pathJoin } from 'path'
+
+import hexToHsl from 'hex-to-hsl'
+
+async function processLineByLine() {
+	const path = pathJoin(__dirname + '../../../src' + '/green-funny.txt')
+	const fileStream = fs.createReadStream(path)
+
+	const rl = readline.createInterface({
+		input: fileStream,
+		crlfDelay: Infinity
+	})
+	// Note: we use the crlfDelay option to recognize all instances of CR LF
+	// ('\r\n') in input.txt as a single line break.
+
+	for await (const line of rl) {
+		// Each line in input.txt will be successively available here as `line`.
+		console.log(`Line from file: ${line} - HSL: ${hexToHsl(line)}`)
+	}
+}
+
+processLineByLine()
