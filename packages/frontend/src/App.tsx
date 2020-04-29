1ffa66486e4beb66d540752acd2c269ea35b6139
luizcieslak <cieslakluiz@gmail.com>
2020-04-29 17:35:32 -0300

    chore(algorithm): using some pre defined colors to better visualize using only shadesMonochrome function

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -5,9 +5,23 @@ import './App.css'
 import tinyColor from 'tinycolor2'
 import { Box, Input, Flex, Text } from '@chakra-ui/core'
 
-function CardColor({ tinyObj, index }: { tinyObj: tinycolor.Instance; index: number }) {
-	console.log('tiny', tinyObj.toString(), tinyObj.toHexString(), tinyObj.toHex8String())
+/**
+ * Create a array of shades from input, change only Lightness in HSL.
+ * @param color hex color
+ */
+function shadesMonochrome(color: string) {
+	const shades = []
 
+	const hsl = tinyColor(color).toHsl()
+	for (let i = 9.5; i >= 0.5; i -= 1) {
+		hsl.l = 0.1 * i
+		shades.push('background-color: ' + tinyColor(hsl).toRgbString())
+	}
+	return shades
+}
+
+function CardColorHex({ hex, index }: { hex: string; index: number }) {
+	const tinyObj = tinyColor(`#${hex}`)
 	return (
 		<Flex direction='column' alignItems='center' p={1} w='120px' border='1px'>
 			<Box w='80px' h='80px' bg={tinyObj.toString()} />
@@ -23,53 +37,73 @@ const App = () => {
 
 	const obj = tinyColor(`#${color}`)
 
-	// darken 4 colors: 600, 700, 800, 900
-	const darkened = Array(4)
-		.fill(0)
-		.map((el, i) => {
-			return obj.clone().darken((i + 1) * 10)
-		})
+	// // darken 4 colors: 600, 700, 800, 900
+	// const darkened = Array(4)
+	// 	.fill(0)
+	// 	.map((el, i) => {
+	// 		return obj.clone().darken((i + 1) * 10)
+	// 	})
 
-	// lighten 4 colors: 100, 200, 300, 400
-	const lightened = Array(4)
-		.fill(0)
-		.map((el, i) => {
-			return obj.clone().lighten((i + 1) * 10)
-		})
+	// // lighten 4 colors: 100, 200, 300, 400
+	// const lightened = Array(4)
+	// 	.fill(0)
+	// 	.map((el, i) => {
+	// 		return obj.clone().lighten((i + 1) * 10)
+	// 	})
 
-	// darken 4 colors: 600, 700, 800, 900
-	const saturated = Array(4)
-		.fill(0)
-		.map((el, i) => {
-			return obj.clone().saturate((i + 1) * 10)
-		})
+	// // darken 4 colors: 600, 700, 800, 900
+	// const saturated = Array(4)
+	// 	.fill(0)
+	// 	.map((el, i) => {
+	// 		return obj.clone().saturate((i + 1) * 10)
+	// 	})
 
-	// lighten 4 colors: 100, 200, 300, 400
-	const desaturated = Array(4)
-		.fill(0)
-		.map((el, i) => {
-			return obj.clone().desaturate((i + 1) * 10)
-		})
+	// // lighten 4 colors: 100, 200, 300, 400
+	// const desaturated = Array(4)
+	// 	.fill(0)
+	// 	.map((el, i) => {
+	// 		return obj.clone().desaturate((i + 1) * 10)
+	// 	})
 
-	// darken 4 colors: 600, 700, 800, 900
-	const bothPlus = Array(4)
-		.fill(0)
-		.map((el, i) => {
-			return obj
-				.clone()
-				.darken((i + 1) * 10)
-				.saturate((i + 1) * 10)
-		})
+	// // darken 4 colors: 600, 700, 800, 900
+	// const bothPlus = Array(4)
+	// 	.fill(0)
+	// 	.map((el, i) => {
+	// 		return obj
+	// 			.clone()
+	// 			.darken((i + 1) * 10)
+	// 			.saturate((i + 1) * 10)
+	// 	})
 
-	// lighten 4 colors: 100, 200, 300, 400
-	const bothMinus = Array(4)
-		.fill(0)
-		.map((el, i) => {
-			return obj
-				.clone()
-				.lighten((i + 1) * 10)
-				.desaturate((i + 1) * 10)
-		})
+	// // lighten 4 colors: 100, 200, 300, 400
+	// const bothMinus = Array(4)
+	// 	.fill(0)
+	// 	.map((el, i) => {
+	// 		return obj
+	// 			.clone()
+	// 			.lighten((i + 1) * 10)
+	// 			.desaturate((i + 1) * 10)
+	// 	})
+
+	const colorsInputArray = shadesMonochrome(`#${color}`)
+
+	// const purple = shadesMonochrome(`#3e2f5b`)
+	// const blueGIS = shadesMonochrome(`#59ccf2`)
+	// const redGIS = shadesMonochrome(`#FC5A5A`)
+	// const greenGIS = shadesMonochrome(`#3dd598`)
+	// const grayGIS = shadesMonochrome(`#B5B5BE`)
+	// const yellowGIS = shadesMonochrome(`#FF933C`)
+	// const darkJungleGreen = shadesMonochrome(`#000F08`)
+
+	const arrayOfArrayPredefinedColors = [
+		shadesMonochrome(`#3e2f5b`),
+		shadesMonochrome(`#59ccf2`),
+		shadesMonochrome(`#FC5A5A`),
+		shadesMonochrome(`#3dd598`),
+		shadesMonochrome(`#B5B5BE`),
+		shadesMonochrome(`#FF933C`),
+		shadesMonochrome(`#000F08`)
+	]
 
 	return (
 		<div className='App'>
@@ -82,13 +116,13 @@ const App = () => {
 					Learn React
 				</a>
 			</header>
-			<Input type='text' value={color} onChange={(e: any) => setColor(e.target.value)} />
-			<br />
-			{color}
-			<br />
-			<CardColor tinyObj={obj} index={4} />
-			<br />
-			<Flex justifyContent='center'>
+			<Box border='1px' p={4} m={4}>
+				<Input type='text' value={color} onChange={(e: any) => setColor(e.target.value)} />
+				<br />#{color}
+				<br />
+				{tinyColor(`#${color}`).toHslString()}
+				<br />
+				{/* <Flex justifyContent='center'>
 				{[...lightened.reverse(), obj, ...darkened].map((color, index) => (
 					<CardColor key={`cc-${index}`} tinyObj={color} index={index} />
 				))}
@@ -104,7 +138,24 @@ const App = () => {
 				{[...bothMinus.reverse(), obj, ...bothPlus].map((color, index) => (
 					<CardColor key={`cc-${index}`} tinyObj={color} index={index} />
 				))}
-			</Flex>
+			</Flex> */}
+				<br />
+				<Flex justifyContent='center'>
+					{colorsInputArray.map((color, index) => (
+						<CardColorHex key={`cc-${index}`} hex={color} index={index} />
+					))}
+				</Flex>
+			</Box>
+
+			{arrayOfArrayPredefinedColors.map((arrayColors, index1) => (
+				<Box key={`b-${index1}`} border='1px' p={4} m={4}>
+					<Flex justifyContent='center'>
+						{arrayColors.map((color, index2) => (
+							<CardColorHex key={`ccx-${index2}`} hex={color} index={index2} />
+						))}
+					</Flex>
+				</Box>
+			))}
 		</div>
 	)
 }
