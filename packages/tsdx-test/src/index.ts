05f8886fdff54be6b4468031e230235aee6eda17
luizcieslak <cieslakluiz@gmail.com>
2020-05-07 17:30:25 -0300

    fix(tsdx): import without ./ was failing

--- packages/tsdx-test/src/index.ts
+++ packages/tsdx-test/src/index.ts
@@ -1,6 +1,10 @@
+import shades from './shades'
+
 export const sum = (a: number, b: number) => {
 	if ('development' === process.env.NODE_ENV) {
 		console.log('boop')
 	}
 	return a + b
 }
+
+export default shades
