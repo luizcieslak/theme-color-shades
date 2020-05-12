73902bd596be2a5f057478d810e5ebe2ee57dad9
luizcieslak <cieslakluiz@gmail.com>
2020-05-12 12:21:23 -0300

    style(components): stack to radio and textarea

--- packages/frontend/src/pages/components.tsx
+++ packages/frontend/src/pages/components.tsx
@@ -7,7 +7,7 @@ import shades, { ColorObj } from 'theme-color-shades'
 import Shades from '../components/Shades'
 import DemoComponents from '../components/DemoComponents'
 import theme, { CustomTheme } from '../theme'
-import { FormLabel, RadioGroup, Radio, Textarea, useClipboard, Button } from '@chakra-ui/core'
+import { FormLabel, RadioGroup, Radio, Textarea, useClipboard, Button, Flex, Stack, Box } from '@chakra-ui/core'
 
 const Components = () => {
 	const [format, setFormat] = useState<string>('object')
@@ -42,19 +42,29 @@ const Components = () => {
 			{/* <Text fontSize='4xl'>{ntcjs.name(`#${color}`)[1]}</Text> */}
 			<Shades originalColor={color} shades={shadesTinyColor as tinyColor.Instance[]} />
 
-			<FormLabel htmlFor='format'>Select output format</FormLabel>
-			<RadioGroup id='format' value={format} onChange={e => setFormat(e.target.value)} spacing={5} isInline>
-				<Radio variantColor='brand' value='object'>
-					Object
-				</Radio>
-				<Radio variantColor='brand' value='array'>
-					Array
-				</Radio>
-			</RadioGroup>
-			<Button py={4} variantColor='brand' onClick={onCopy}>
-				{hasCopied ? 'Copied' : 'Copy'}
-			</Button>
-			<Textarea value={JSON.stringify(format === 'array' ? shadesArray : shadesObject, null, 2)} minH='280px' />
+			<Stack flexWrap='wrap' py={2} isInline spacing={16}>
+				<Stack spacing={4}>
+					<Box>
+						<FormLabel htmlFor='format'>Select output format</FormLabel>
+						<RadioGroup id='format' value={format} onChange={e => setFormat(e.target.value)} spacing={5} isInline>
+							<Radio variantColor='brand' value='object'>
+								Object
+							</Radio>
+							<Radio variantColor='brand' value='array'>
+								Array
+							</Radio>
+						</RadioGroup>
+					</Box>
+					<Button py={4} variantColor='brand' onClick={onCopy}>
+						{hasCopied ? 'Copied' : 'Copy'}
+					</Button>
+				</Stack>
+				<Textarea
+					value={JSON.stringify(format === 'array' ? shadesArray : shadesObject, null, 2)}
+					minH='280px'
+					maxW='250px'
+				/>
+			</Stack>
 			<DemoComponents />
 		</Layout>
 	)
