e5f3393033be0a2c5ba48dcd3da824cc43e92ee0
luizcieslak <cieslakluiz@gmail.com>
2020-05-17 16:24:07 -0300

    feat(header): finish index header

--- packages/frontend/src/components/header.tsx
+++ packages/frontend/src/components/header.tsx
@@ -1,8 +1,10 @@
-import { Link, useStaticQuery, graphql } from 'gatsby'
+import { Link as GatsbyLink, useStaticQuery, graphql } from 'gatsby'
 import React from 'react'
-import { Box, Flex, Text } from '@chakra-ui/core'
+import { Box, Flex, Text, Stack, Link } from '@chakra-ui/core'
 import GatsbyImage from 'gatsby-image'
 import { GatsbyIconQuery } from '../generated/graphql'
+
+import { IoLogoGithub } from 'react-icons/io'
 
 // const iconQuery = graphql`
 // 	query GatsbyIcon {
@@ -35,16 +37,23 @@ const Header: React.FunctionComponent<HeaderProps> = props => {
 	// const data: GatsbyIconQuery = useStaticQuery(iconQuery)
 	// console.log('data icon header', data)
 	return (
-		<Box color='white' bg='white' px={[2, 12]} py={4} boxShadow='md'>
+		<Flex justifyContent='space-between' align='center' bg='white' px={[2, 12]} py={4} boxShadow='sm'>
 			<Flex alignItems='center'>
 				{/* <Box width='100px' pr='4'>
 					<GatsbyImage fluid={data.file.childImageSharp.fluid} />
 				</Box> */}
-				<Text color='brand.900' w='60px' textTransform='lowercase'>
-					<Link to='/'>{props.siteTitle}</Link>
+				<Text color='gray.900' w='60px' textTransform='lowercase'>
+					<GatsbyLink to='/'>{props.siteTitle}</GatsbyLink>
 				</Text>
 			</Flex>
-		</Box>
+
+			<Stack isInline spacing={[4, 8]}>
+				<Link color='gray.900'>CLI package</Link>
+				<Link color='brand.800' _hover={{ color: 'brand.900' }}>
+					<IoLogoGithub size='25' />
+				</Link>
+			</Stack>
+		</Flex>
 	)
 }
 
