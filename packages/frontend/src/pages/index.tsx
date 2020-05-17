0f2d8e5099c379b4b0d20e5f150528616268b472
luizcieslak <cieslakluiz@gmail.com>
2020-05-16 21:26:53 -0300

    feat(index): grid for logo and input, going to components

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 import tinyColor from 'tinycolor2'
-import { Box, Flex, Text, Input } from '@chakra-ui/core'
+import { Box, Flex, Text, Input, Stack, SimpleGrid, Button, FormLabel } from '@chakra-ui/core'
 import shades, { ColorObj } from 'theme-color-shades'
 import CardColorHex from '../components/ CardColorHex'
-import Logo from './Logo'
+import Logo from '../components/Logo'
 import theme, { CustomTheme } from '../theme'
+import { navigate } from 'gatsby'
 
 // interface IProps {
 // 	data: GatsbyAstronautQuery
@@ -61,25 +62,45 @@ interface ColorObject {
 const IndexPage: React.FC = () => {
 	const [colorFromLogo, setColorFromLogo] = useState('')
 
-	const shadesObject = shades({ color: colorFromLogo, hue: true, saturation: true, outputFormat: 'object' })
+	const [themeWithNewColor, setThemeWithNewColor] = useState<CustomTheme>(theme)
 
-	const themeWithNewColor: CustomTheme = {
-		...theme,
-		colors: {
-			...theme.colors,
-			brand: shadesObject as ColorObj
+	useEffect(() => {
+		if (colorFromLogo !== '') {
+			const shadesObject = shades({ color: colorFromLogo, hue: true, saturation: true, outputFormat: 'object' })
+			setThemeWithNewColor({
+				...theme,
+				colors: {
+					...theme.colors,
+					brand: shadesObject as ColorObj
+				}
+			})
 		}
-	}
+	}, [colorFromLogo])
 
+	console.log('color', colorFromLogo)
 	return (
 		<Layout theme={themeWithNewColor}>
 			{/* <Layout> */}
 			<SEO keywords={[`gatsby`, `colors`, `react`]} />
-			index
-			<Box w='300px'>
-				<Logo setColor={setColorFromLogo} />
-			</Box>
-			<Text color='brand.500'>Generate a group of color shades ready to be used in your UI library.</Text>
+			<SimpleGrid columns={2} spacing={10} minChildWidth={['300px', '400px']}>
+				<Box maxW={['300px', '400px']}>
+					<Logo setColor={setColorFromLogo} />
+				</Box>
+				<Stack spacing={2}>
+					<Text color='brand.900' fontSize={['2xl', '4xl', '5xl']}>
+						Generate a group of color shades ready to be used in your UI library.
+					</Text>
+					<FormLabel htmlFor='color' fontSize='xl'>
+						Try it out:
+					</FormLabel>
+					<Stack isInline spacing={4}>
+						<Input placeholder='an HEX color' defaultValue={colorFromLogo} w='50%' />
+						<Button variantColor='brand' onClick={() => navigate(`components?color=${colorFromLogo.split('#')[1]}`)}>
+							Go!
+						</Button>
+					</Stack>
+				</Stack>
+			</SimpleGrid>
 		</Layout>
 	)
 }
