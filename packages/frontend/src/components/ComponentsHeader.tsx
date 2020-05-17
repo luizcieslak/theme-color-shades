fa7f6eeab577e401e8af4de49da07c68cc3e4386
luizcieslak <cieslakluiz@gmail.com>
2020-05-17 17:26:26 -0300

    feat(ComponentsHeader): add stripe, fixed

--- packages/frontend/src/components/ComponentsHeader.tsx
+++ packages/frontend/src/components/ComponentsHeader.tsx
@@ -1,11 +1,14 @@
 import { Link as GatsbyLink, graphql, useStaticQuery } from 'gatsby'
 import React from 'react'
-import { Box, Flex, Text, Stack, Link } from '@chakra-ui/core'
+import { Box, Flex, Text, Stack, Link, useTheme } from '@chakra-ui/core'
 
 import { IoLogoGithub } from 'react-icons/io'
 
 import { LogoIconQuery } from '../generated/graphql'
 import GatsbyImage from 'gatsby-image'
+import Shades from './Shades'
+import ShadesStripeHeader from './ShadesStripeHeader'
+import { CustomTheme } from '../theme'
 
 interface HeaderProps {
 	siteTitle: string
@@ -35,9 +38,37 @@ const iconQuery = graphql`
 `
 const ComponentsHeader: React.FunctionComponent<HeaderProps> = props => {
 	const data: LogoIconQuery = useStaticQuery(iconQuery)
-	console.log('data icon header', data)
+
+	const theme = useTheme() as CustomTheme
+
+	// Listen to page scroll, show shades stripe when scrolled
+	const [pageYOffset, setPageYOffset] = React.useState(0)
+	const scrollPage = () => {
+		if (typeof window !== 'undefined') {
+			setPageYOffset(window.pageYOffset)
+			window.removeEventListener('scroll', scrollPage)
+		}
+	}
+	React.useEffect(() => {
+		if (typeof window !== 'undefined') {
+			window.addEventListener('scroll', scrollPage, { passive: true })
+		}
+		return () => window.removeEventListener('scroll', scrollPage)
+	}, [pageYOffset])
+
 	return (
-		<Flex justifyContent='space-between' align='center' bg='white' px={[2, 12]} py={4} boxShadow='sm'>
+		<Flex
+			pos='fixed'
+			w='100vw'
+			top='0'
+			zIndex={2}
+			justifyContent='space-between'
+			align='center'
+			bg='white'
+			px={[2, 12]}
+			py={4}
+			boxShadow='sm'
+		>
 			<GatsbyLink to='/'>
 				<Flex alignItems='center'>
 					<Box width='40px' pr='4'>
@@ -50,9 +81,13 @@ const ComponentsHeader: React.FunctionComponent<HeaderProps> = props => {
 			</GatsbyLink>
 
 			<Stack isInline spacing={[4, 8]}>
-				<Link color='brand.800' _hover={{ color: 'brand.900' }}>
-					<IoLogoGithub size='25' />
-				</Link>
+				{pageYOffset > 228 ? (
+					<ShadesStripeHeader shades={Object.values(theme.colors.brand)} />
+				) : (
+					<Link color='brand.800' _hover={{ color: 'brand.900' }}>
+						<IoLogoGithub size='25' />
+					</Link>
+				)}
 			</Stack>
 		</Flex>
 	)
