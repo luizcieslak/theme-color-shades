179af3ec7f8825f1adcf89af60e12399ef9f0dac
luizcieslak <cieslakluiz@gmail.com>
2020-05-18 21:08:56 -0300

    chore(manifest): add info

--- packages/frontend/gatsby-config.js
+++ packages/frontend/gatsby-config.js
@@ -28,13 +28,13 @@ module.exports = {
 		{
 			resolve: `gatsby-plugin-manifest`,
 			options: {
-				name: `gatsby-starter-default`,
-				short_name: `starter`,
+				name: `theme color shades`,
+				short_name: `tcs`,
 				start_url: `/`,
-				background_color: `#663399`,
-				theme_color: `#663399`,
+				background_color: `#0dd8a4`,
+				theme_color: `#0dd8a4`,
 				display: `minimal-ui`,
-				icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+				icon: `src/images/logo.png` // This path is relative to the root of the site.
 			}
 		},
 		// {
